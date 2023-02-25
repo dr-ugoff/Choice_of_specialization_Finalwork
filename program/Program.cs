64b2764767b44eb2d2680c1d63b767ac6a76695f
Andrey <dr_ugoff@mail.ru>
2023-02-25 19:31:39 +0700

    Add program

---
+++ program/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array = InputArray();
+string[] result = OutputArray(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] OutputArray(string[] input, int n) {
+    string[] output = new string[SearchLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int SearchLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] InputArray() {
+    Console.Write("Введите значения через запятую: ");
+    return Console.ReadLine().Split(",");
+}
